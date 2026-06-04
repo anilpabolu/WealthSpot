0bb145f6ae001e3927dc8c4ad7838481e3862997
anilpabolu <anilpabolu.mastek@gmail.com>
2026-06-04 15:30:35 +0530

    fix: resolve alembic multiple heads by making 069 revise 068

--- services/api/alembic/versions/069_founding_team.py
+++ services/api/alembic/versions/069_founding_team.py
@@ -16,7 +16,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "069_founding_team"
-down_revision: Union[str, None] = "aa16902c5305"
+down_revision: Union[str, None] = "068_safety_net_columns"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
